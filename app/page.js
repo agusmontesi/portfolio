3bcc374eb1c4a7d656677989137f10f01d2b16ce
agustin <agustmontesi@gmail.com>
2024-04-27 04:52:06 -0300

    ready for deploy

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -23,10 +24,10 @@ async function getData() {
 };
 
 export default async function Home() {
-  const blogs = await getData();
 
   return (
     <>
+      <SpeedInsights />
       <HeroSection />
       <AboutSection />
       <Experience />
